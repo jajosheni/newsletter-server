2f575b11bdeb586c2987dfaa1c4ea848940dcba1
jajosheni <shen.i@live.com>
2019-04-03 05:16:33 +0300

    remove unnecessary files

--- routes/list.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET Articles page. */
-router.get('/', function(req, res, next) {
-    res.render('list', { title: 'Articles' });
-});
-
-module.exports = router;
